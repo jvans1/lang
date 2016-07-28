8369ff4ef839a1a5cb3c878b51601293c860a45b
James Vanneman <jvannem@gmail.com>
2016-07-28 10:39:52 +0100

    remove cgen start over

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,5 @@
 module Main where
-import CodeGeneration.CCompiler(compile)
 import System.Environment(getArgs)
 
 main :: IO ()
-main = do
-  (f:_) <- getArgs
-  code <- readFile f
-  let res = compile f
-  writeFile "output.c" res
+main = error "Main is undefined"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,8 @@
 import qualified ParserSpec
-import qualified CodeGenerationSpec
 import qualified TypeResolutionSpec
-import qualified CCompilerSpec
 
 import Test.Hspec
 main :: IO ()
 main = hspec $ do
   ParserSpec.run
-  CodeGenerationSpec.run
   TypeResolutionSpec.run
-  CCompilerSpec.run
